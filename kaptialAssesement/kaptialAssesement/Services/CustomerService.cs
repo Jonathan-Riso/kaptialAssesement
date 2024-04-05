17bd8289c6175e003a8df6e3ef0d9c9414ddbcb1
Jonathan <riso.jona10@gmail.com>
2024-04-04 21:43:30 -0400

    Implement Field Validation for Customers

--- kaptialAssesement/kaptialAssesement/Services/CustomerService.cs
+++ kaptialAssesement/kaptialAssesement/Services/CustomerService.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace kaptialAssesement.Services
 {
@@ -33,16 +35,20 @@ namespace kaptialAssesement.Services
 
         public async Task<HttpStatusCode> CreateCustomer(Customer customer)
         {
+			if (_isNotValidCustomer(customer)) { return HttpStatusCode.BadRequest; }
+
             _context.Customers.Add(customer);
             await _context.SaveChangesAsync();
 
-            return HttpStatusCode.Accepted;
+            return HttpStatusCode.Created;
         }
 
         public async Task<HttpStatusCode> EditCustomer(int? Id, Customer customer)
         {
             var found_customer = await _context.Customers.FindAsync(Id);
             if (found_customer == null) { return HttpStatusCode.NotFound; }
+
+			if (_isNotValidCustomer(customer)) { return HttpStatusCode.BadRequest; }
 
 			found_customer.PhoneNumber = customer.PhoneNumber;
 			found_customer.Name = customer.Name;
@@ -51,7 +57,7 @@ namespace kaptialAssesement.Services
 
 			await _context.SaveChangesAsync();
 
-            return HttpStatusCode.Accepted;
+            return HttpStatusCode.Created;
         }
 
         public async Task<HttpStatusCode> DeleteCustomer(int Id)
@@ -62,7 +68,23 @@ namespace kaptialAssesement.Services
             _context.Customers.Remove(customer); 
             await _context.SaveChangesAsync();
 
-            return HttpStatusCode.Accepted;
+            return HttpStatusCode.Created;
         }
+
+        private bool _isNotValidCustomer(Customer customer)
+        {
+            if (customer == null) return true;
+            if (customer.PhoneNumber == null || customer.PhoneNumber.Length != 12)
+                return true;
+			if (customer.Name == null || customer.Name.Length <= 0 || customer.Name.Length >= 200)
+				return true;
+
+			if (customer.Email == null || customer.Email.Length <= 0 || customer.Email.Length >= 200
+                || !MailAddress.TryCreate(customer.Email, out var address))
+                return true;
+
+			return false;
+
+		}
     }
 }
